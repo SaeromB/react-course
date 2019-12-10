1da1a76ff71c505262cf0d3a3332d49b270e6001
Saerom <saerombang11@gmail.com>
2019-12-10 20:13:22 +0900

    added toggle.js

--- src/index.js
+++ src/index.js
@@ -2,10 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 // import App from './App';
-import Counter from './counter-example';
+// import Counter from './counter-example';
+import VisibilityToggle from './toggle';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<Counter/>, document.getElementById('root'));
+ReactDOM.render(<VisibilityToggle/>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
