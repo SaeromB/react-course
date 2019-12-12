555fc7954cdeb4fe62a4765dcdec84201cd092b8
Saerom <saerombang11@gmail.com>
2019-12-12 11:35:13 +0900

    changed Header to stateless component

--- src/indecision.js
+++ src/indecision.js
@@ -56,16 +56,18 @@ class Indecision extends Component {
   }
 }
 
+const Header = (props) => {
+  return (
+    <div>
+      <h1>{props.title}</h1>
+      {props.subtitle && <h2>{props.subtitle}</h2>}
+    </div>
+  )
+}
 
-class Header extends Component {
-  render() {
-    return(
-      <div>
-        <h1>{this.props.title}</h1>
-        <h2>{this.props.subtitle}</h2>
-      </div>
-    )
-  }
+// give default to the title
+Header.defaultProps = {
+  title: 'Indecision'
 }
 
 // class Action extends Component {
